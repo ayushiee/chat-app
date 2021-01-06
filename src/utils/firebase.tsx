fd90b0014125eea340bf09a77404cce3f22fece2
Ayushi <ayushigupta8@gmail.com>
2021-01-06 19:04:17 +0530

    feat: added firebase

---
+++ src/utils/firebase.tsx
@@ -0,0 +1,11 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+import config from '../config.json';
+
+const firebaseConfig = config.firebase;
+
+firebase.initializeApp(firebaseConfig);
+export const auth:any = firebase.auth();
+export const firestore:any = firebase.firestore();
