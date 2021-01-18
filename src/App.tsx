a5468b7b70d912d5515b6ddcca81cfea3989d573
Ayushi <ayushigupta8@gmail.com>
2021-01-18 22:25:48 +0530

    feat: added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import SignUp from './pages/Signup/Signup';
-import './App.scss';
-import { AuthProvider } from './utils/auth';
+import { Dashboard, SignUp, Login } from './pages';
+import { AuthProvider } from './context/auth';
+import { ROUTES } from './utils/constants';
 
-function App() {
+function App(): React.ReactElement {
   return (
-    <AuthProvider>
-      <div className='App'>
-        <SignUp />
-      </div>
-    </AuthProvider>
+    <Router>
+      <AuthProvider>
+        <Switch>
+          <Route path={ROUTES.SIGNUP} component={SignUp} exact />
+          <Route path={ROUTES.LOGIN} component={Login} exact />
+          <Route path={ROUTES.DASHBOARD} component={Dashboard} exact />
+        </Switch>
+      </AuthProvider>
+    </Router>
   );
 }
 
---
+++ src/utils/constants.ts
@@ -0,0 +1,6 @@
+export enum ROUTES {
+    HOME = '/',
+    LOGIN = '/login',
+    SIGNUP = '/signup',
+    DASHBOARD = '/dashboard'
+}
